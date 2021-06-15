7c58da755a8fcc5a6e9cfdec1ec5b7756fef845a
sebafurego <sebafurego@yandex.ru>
2021-06-15 18:36:53 +0300

    Delete element

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 
-const App = () => {
+export default class App extends Component {
+  state = {
+    todoData: [
+      {label: 'Drink Coffee', important: false, id: 1},
+      {label: 'Make Awesome App', important: true, id: 2},
+      {label: 'Have a lunch', important: false, id: 3},
+    ]
+  };
 
-  const todoData = [
-    {label: 'Drink Coffee', important: false, id: 1},
-    {label: 'Make Awesome App', important: true, id: 2},
-    {label: 'Have a lunch', important: false, id: 3},
-  ]
+  deleteItem = (id) => {
+    this.setState( ({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
 
-  return (
-    <div className="container">
-      <AppHeader />
-      <SearchPanel />
-      <TodoList 
-        todos={todoData} 
-        onDeleted={(id) => console.log('Deleted', id)}/>
-    </div>
-  );
+      const newArrey = [ 
+        ...todoData.slice(0, idx), 
+        ...todoData.slice(idx + 1)
+      ];
+
+      return {
+        todoData: newArrey
+      };
+    });
+  };
+
+  render () {
+    return (
+      <div className="container">
+        <AppHeader />
+        <SearchPanel />
+        <TodoList 
+          todos={this.state.todoData} 
+          onDeleted={this.deleteItem}/>
+      </div>
+    );
+  };
 };
-
-export default App;
